a780287ca05e3878f52f7a1ee3d784b4f506988f
Lukas P <lukas.ring@gmail.com>
2020-11-05 19:45:22 +0200

    functions with promises remade with async await instead of .then

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,55 +8,53 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-exports.getEditProduct = (req, res, next) => {
+exports.getEditProduct = async (req, res, next) => {
   const editMode = !!req.query.edit; // sitas tik kad query params panaudot
   if (!editMode) return res.redirect("/");
   const prodId = req.params.productId;
   // Product.findByPk(prodId)
 
-  req.user
-    .getProducts({ where: { id: prodId } })
-    .then(([product]) => {
-      if (!product) return res.redirect("/");
+  try {
+    const products = await req.user.getProducts({ where: { id: prodId } });
+    const product = products[0];
+    if (!product) return res.redirect("/");
 
-      res.render("admin/edit-product", {
-        docTitle: "Edit Product",
-        path: "/admin/edit-product",
-        product: product,
-        editing: editMode,
-      });
-    })
-    .catch((err) => {
-      console.log("!!! ERROR !!! controllers/admin.js -> getEditProduct");
-      console.log(err);
+    res.render("admin/edit-product", {
+      docTitle: "Edit Product",
+      path: "/admin/edit-product",
+      product: product,
+      editing: editMode,
     });
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/admin.js -> getEditProduct");
+    console.log(err);
+  }
 };
 
-exports.postEditProduct = (req, res, next) => {
+exports.postEditProduct = async (req, res, next) => {
   const title = req.body.title;
   const imageURL = req.body.imageURL;
   const price = +req.body.price;
   const description = req.body.description;
   const id = req.body.productId;
 
-  Product.findByPk(id)
-    .then((product) => {
-      product.title = title;
-      product.imageUrl = imageURL;
-      product.price = price;
-      product.description = description;
-      return product.save();
-    })
-    .then(() => {
+  try {
+    const product = await Product.findByPk(id);
+    product.title = title;
+    product.imageUrl = imageURL;
+    product.price = price;
+    product.description = description;
+    const productSaved = await product.save();
+    if (productSaved) {
       res.redirect("/admin/products");
-    })
-    .catch((err) => {
-      console.log("!!! ERROR !!! controllers/admin.js -> postEditProduct");
-      console.log(err);
-    });
+    }
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/admin.js -> postEditProduct");
+    console.log(err);
+  }
 };
 
-exports.postAddProduct = (req, res, next) => {
+exports.postAddProduct = async (req, res, next) => {
   const title = req.body.title;
   const imageURL = req.body.imageURL;
   const price = +req.body.price;
@@ -66,52 +64,43 @@ exports.postAddProduct = (req, res, next) => {
   User objekta storinam per middleware app.js faile,
   sequelize jam sukuria createProduct, kuris susieja userId su Product
   */
-  req.user
-    .createProduct({
+  try {
+    const productCreated = await req.user.createProduct({
       title: title,
       price: price,
       imageUrl: imageURL,
       description: description,
-    })
-    .then(() => {
-      console.log("Product Created!");
-      res.redirect("/admin/products");
-    })
-    .catch((err) => {
-      console.log("!!! ERROR !!! controllers/admin.js postAddProduct");
-      console.log(err);
     });
-  // console.log(req.body);
+    if (productCreated) res.redirect("/admin/products");
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/admin.js postAddProduct");
+    console.log(err);
+  }
 };
 
-exports.getAdminProducts = (req, res, next) => {
-  // Product.findAll()
-  req.user
-    .getProducts()
-    .then((products) => {
-      res.render("admin/products", {
-        products: products,
-        path: "/admin/products",
-        docTitle: "Product List",
-      });
-    })
-    .catch((err) => {
-      console.log("!!! ERROR !!! controllers/admin.js -> getAdminProducts");
-      console.log(err);
+exports.getAdminProducts = async (req, res, next) => {
+  try {
+    const products = await req.user.getProducts();
+    res.render("admin/products", {
+      products: products,
+      path: "/admin/products",
+      docTitle: "Product List",
     });
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/admin.js -> getAdminProducts");
+    console.log(err);
+  }
 };
 
-exports.postDeleteProduct = (req, res, next) => {
+exports.postDeleteProduct = async (req, res, next) => {
   const id = req.body.productId;
   // Product.destroy({where: {id: id}});
-  Product.findByPk(id)
-    .then((product) => product.destroy())
-    .then(() => {
-      console.log("Product deleted...");
-      res.redirect("/admin/products");
-    })
-    .catch((err) => {
-      console.log("!!! ERROR !!! controllers/admin.js -> postDeleteProduct");
-      console.log(err);
-    });
+  try {
+    const product = await Product.findByPk(id);
+    const productDeleted = await product.destroy();
+    if (productDeleted) res.redirect("/admin/products");
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/admin.js -> postDeleteProduct");
+    console.log(err);
+  }
 };
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,117 +1,90 @@
 const Product = require("../models/product");
 
-exports.getProducts = (req, res, next) => {
-  Product.findAll()
-    .then((products) => {
-      res.render("shop/product-list", {
-        products: products,
-        docTitle: "All Products",
-        path: "/products",
-      });
-    })
-    .catch((err) =>
-      console.log("!!! ERROR !!! controllers/shop.js -> getProducts", err)
-    );
+exports.getProducts = async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.render("shop/product-list", {
+      products: products,
+      docTitle: "All Products",
+      path: "/products",
+    });
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/shop.js -> getProducts", err);
+  }
 };
 
-exports.getProduct = (req, res, next) => {
+exports.getProduct = async (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
-      res.render("shop/product-detail", {
-        docTitle: `${product.title} details`,
-        path: "/products",
-        product: product,
-      });
-    })
-    .catch((err) => {
-      console.log("!!! ERROR !!! controlers/shop.js -> getProduct");
-      console.log(err);
+  try {
+    const product = await Product.findByPk(prodId);
+    res.render("shop/product-detail", {
+      docTitle: `${product.title} details`,
+      path: "/products",
+      product: product,
     });
-
-  // Alteernatyvi sintakse naudojant find all
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(([product]) => {
-  //     res.render("shop/product-detail", {
-  //       docTitle: `${product.title} details`,
-  //       path: "/products",
-  //       product: product,
-  //     });
-  //   })
+  } catch (err) {
+    console.log("!!! ERROR !!! controlers/shop.js -> getProduct");
+    console.log(err);
+  }
 };
 
-exports.getCart = (req, res, next) => {
-  req.user
-    .getCart()
-    .then((cart) => {
-      console.log(cart);
-      return cart.getProducts();
-    })
-    .then((products) => {
-      console.log(products);
-      res.render("shop/cart", {
-        docTitle: "Cart",
-        path: "/cart",
-        products: products,
-      });
-    })
-    .catch((err) => {
-      console.log("!!! ERROR !!! controllers/shop.js -> getCart");
-      console.log(err);
+exports.getCart = async (req, res, next) => {
+  try {
+    const cart = await req.user.getCart();
+    const products = await cart.getProducts();
+    res.render("shop/cart", {
+      docTitle: "Cart",
+      path: "/cart",
+      products: products,
     });
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/shop.js -> getCart");
+    console.log(err);
+  }
 };
 
-exports.postCart = (req, res, next) => {
+exports.postCart = async (req, res, next) => {
   const prodId = req.body.productId;
-  let fetchedCart;
-  let newQuantity = 1;
-  req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then((products) => {
-      let product;
-      if (products.length > 0) {
-        product = products[0];
-      }
 
-      if (product) {
-        const oldQuantity = product.cartItem.quantity;
-        newQuantity = oldQuantity + 1;
-        return product;
-      }
-      return Product.findByPk(prodId);
-    })
-    .then((product) => {
-      fetchedCart.addProduct(product, {
-        through: { quantity: newQuantity },
-      });
-    })
-    .then(() => res.redirect("/cart"))
-    .catch((err) => {
-      console.log("!!! ERROR !!! controllers/shop.js -> postCart");
-      console.log(err);
+  try {
+    let newQuantity = 1;
+    let product;
+
+    const cart = await req.user.getCart();
+    const products = await cart.getProducts({ where: { id: prodId } });
+    if (products.length > 0) product = products[0];
+
+    if (product) {
+      const oldQuantity = product.cartItem.quantity;
+      newQuantity = oldQuantity + 1;
+    } else {
+      product = await Product.findByPk(prodId);
+    }
+
+    const productAdded = cart.addProduct(product, {
+      through: { quantity: newQuantity },
     });
+
+    if (productAdded) res.redirect("/cart");
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/shop.js -> postCart");
+    console.log(err);
+  }
 };
 
-exports.postDeleteCartItem = (req, res, next) => {
+exports.postDeleteCartItem = async (req, res, next) => {
   const prodId = req.body.productId;
-  req.user
-    .getCart()
-    .then((cart) => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then((products) => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
-    .then(() => res.redirect("/cart"))
-    .catch((err) => {
-      console.log("!!! Error !!! controllers/shop.js -> postDeleteCartItem");
-      console.log(err);
-    });
+  try {
+    const cart = await req.user.getCart();
+    const products = await cart.getProducts({ where: { id: prodId } });
+    const product = products[0];
+    const productDestroyed = await product.cartItem.destroy();
+
+    if (productDestroyed) res.redirect("/cart");
+  } catch (err) {
+    console.log("!!! Error !!! controllers/shop.js -> postDeleteCartItem");
+    console.log(err);
+  }
 };
 
 exports.getCheckoutItems = (req, res, next) => {
@@ -121,61 +94,50 @@ exports.getCheckoutItems = (req, res, next) => {
   });
 };
 
-exports.getIndex = (req, res, next) => {
-  Product.findAll()
-    .then((products) => {
-      res.render("shop/index", {
-        products: products,
-        docTitle: "Shop",
-        path: "/",
-      });
-    })
-    .catch((err) => console.log("!!! ERROR !!!", err));
-};
-
-exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-  req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then((products) => {
-      console.log(products);
-      return req.user
-        .createOrder()
-        .then((order) => {
-          return order.addProducts(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch((err) => console.log(err));
-    })
-    .then(() => {
-      return fetchedCart.setProducts(null);
-    })
-    .then(() => {
-      res.redirect("/orders");
-    })
-    .catch((err) => {
-      console.log("!!! ERROR !!! controllers/shop.js -> postOrder");
-      console.log(err);
+exports.getIndex = async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.render("shop/index", {
+      products: products,
+      docTitle: "Shop",
+      path: "/",
     });
+  } catch (err) {
+    console.log("!!! ERROR !!!", err);
+  }
 };
 
-exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders({ include: ["products"] })
-    .then((orders) => {
-      res.render("shop/orders", {
-        orders: orders,
-        docTitle: "Orders",
-        path: "/orders",
-      });
-    })
-    .catch((err) => console.log(err));
+exports.postOrder = async (req, res, next) => {
+  try {
+    const cart = await req.user.getCart();
+    const products = await cart.getProducts();
+    const order = await req.user.createOrder();
+    const productsOrdered = await order.addProducts(
+      products.map((product) => {
+        product.orderItem = { quantity: product.cartItem.quantity };
+        return product;
+      })
+    );
+    let cartEmptied;
+    console.log(productsOrdered);
+    if (productsOrdered) cartEmptied = cart.setProducts(null);
+    if (cartEmptied) res.redirect("/orders");
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/shop.js -> postOrder");
+    console.log(err);
+  }
+};
+
+exports.getOrders = async (req, res, next) => {
+  try {
+    const orders = await req.user.getOrders({ include: ["products"] });
+    res.render("shop/orders", {
+      orders: orders,
+      docTitle: "Orders",
+      path: "/orders",
+    });
+  } catch (err) {
+    console.log("!!! ERROR !!! controllers/shop.js -> getOrders");
+    console.log(err);
+  }
 };
